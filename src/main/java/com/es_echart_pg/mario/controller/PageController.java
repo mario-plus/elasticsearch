9d7c38008e24e341c05503329933005d65c03599
mario <1441349884@qq.com>
2020-11-17 00:01:35 +0800

    修改bug

--- src/main/java/com/es_echart_pg/mario/controller/PageController.java
+++ src/main/java/com/es_echart_pg/mario/controller/PageController.java
@@ -1,18 +1,26 @@
 package com.es_echart_pg.mario.controller;
 
+import com.es_echart_pg.mario.pageUtil.ReptileData;
 import com.es_echart_pg.mario.service.ContentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class PageController {
 
 
-    @Autowired
-    ContentService contentService;
+    final ContentService contentService;
+    final ReptileData reptileData;
+    public PageController(ContentService contentService, ReptileData reptileData) {
+        this.contentService = contentService;
+        this.reptileData = reptileData;
+    }
 
     @GetMapping({"/index","/"})
     public String indexPage(){
@@ -20,12 +28,29 @@ public class PageController {
     }
 
 
+    /*
+    * 将爬到的数据放入es中，入库
+    * */
     @ResponseBody
-    @GetMapping("/getData")
-    public Boolean getData(@RequestParam(name = "keyword") String keyword) throws Exception {
-        System.out.println(contentService);
+    @GetMapping("/putDataToEs")
+    public Boolean putDataToEs(@RequestParam(name = "keyword") String keyword) throws Exception {
         Boolean flag = contentService.parseContent(keyword);
         return flag;
     }
 
+    /*
+    * 从es中搜索数据
+    * */
+    @ResponseBody
+    @GetMapping("/searchDataFormEs")
+    public List<Map<String,Object>> searchDataFormEs(@RequestParam(name = "keyword") String keyword) {
+        System.out.println("----------------------------");
+        List<Map<String, Object>> stringObjectMap = null;
+        try {
+            stringObjectMap = contentService.getStringObjectMap(keyword);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringObjectMap;
+    }
 }
--- src/main/java/com/es_echart_pg/mario/service/ContentService.java
+++ src/main/java/com/es_echart_pg/mario/service/ContentService.java
@@ -7,31 +7,67 @@ import com.es_echart_pg.mario.pageUtil.ReptileData;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ContentService {
-    @Autowired
-    RestHighLevelClient restHighLevelClient;
 
-    @Autowired
-    ReptileData reptileData;
+    //region 注入类
+    private final RestHighLevelClient restHighLevelClient;
+    private final ReptileData reptileData;
+    public ContentService(RestHighLevelClient restHighLevelClient, ReptileData reptileData) {
+        this.restHighLevelClient = restHighLevelClient;
+        this.reptileData = reptileData;
+    }
+    //endregion
 
+    //region 将数据插入es中
     public Boolean parseContent(String keywords) throws Exception{
         List<BookContent> bookContents = reptileData.getData(keywords);
         BulkRequest bulkRequest = new BulkRequest();
         bulkRequest.timeout("3m");
         bookContents.forEach(bookContent -> {
+            System.out.println(bookContent);
             bulkRequest.add(new IndexRequest("jd_goods").source(JSON.toJSONString(bookContent), XContentType.JSON));
         });
         BulkResponse bulk = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
         return !bulk.hasFailures();
     }
+    //endregion
 
+    //region 查询数据
+    public  List<Map<String, Object>> getStringObjectMap(String keyword) throws IOException {
+        SearchRequest searchRequest = new SearchRequest("jd_goods");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("bookName", keyword);
+        searchSourceBuilder.query(termQueryBuilder).timeout(new TimeValue(60, TimeUnit.SECONDS));
+        searchSourceBuilder.size(30);//默认十条
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+        ArrayList list = new ArrayList();
+        for (SearchHit searchHit:searchResponse.getHits().getHits()) {
+            System.out.println(searchHit);
+            Map<String, Object> sourceAsMap = searchHit.getSourceAsMap();
+            System.out.println(sourceAsMap);
+            list.add(sourceAsMap);
+        }
+        return list;
+    }
+    //endregion
 }
